0a5fd20d06e8faf2c32ebc2091173648cf81c173
James H. <156470182+jmes2build@users.noreply.github.com>
2026-09-22 14:16:58 -0600

    Add backend csv and cuda code
    List of GGML operations and backend support status.
    
    How to add a backend to this table:
    Run test-backend-ops support --output csv with your backend name and redirect output to a csv file in docs/ops/ (e.g., docs/ops/CUDA.csv)
    Regenerate /docs/ops.md via ./scripts/create_ops_docs.py
    Legend:
    
    ✅ Fully supported by this backend
    🟡 Partially supported by this backend
    ❌ Not supported by this backend

--- main.cpp
+++ main.cpp
@@ -44,3 +44,45 @@ std::wstring windowName;
 	std::wcout << L"Process ID of " << windowName.c_str() << L" is: " << *processID << std::endl;
 
 	system("PAUSE");
+
+bool llama_adapter_cvec::apply(
+        const llama_model & model,
+        const float * data,
+        size_t len,
+        int32_t n_embd,
+        int32_t il_start,
+        int32_t il_end) {
+    const auto & hparams = model.hparams;
+
+    if (data == nullptr) {
+        // disable the current control vector (but leave allocated for later)
+        layer_start = -1;
+        layer_end   = -1;
+        return true;
+    }
+
+    if (n_embd != (int) hparams.n_embd) {
+        LLAMA_LOG_ERROR("%s: control vector n_embd does not match model\n", __func__);
+        return false;
+    }
+
+    if (tensors.empty()) {
+        if (!init(model)) {
+            return false;
+        }
+    }
+
+    layer_start = il_start;
+    layer_end   = il_end;
+
+    for (size_t il = 1; il < hparams.n_layer(); il++) {
+        assert(tensors[il] != nullptr);
+
+        const size_t off = n_embd * (il - 1); // buffer doesn't have data for layer 0, since it's never present
+        if (off + n_embd <= len) {
+            ggml_backend_tensor_set(tensors[il], data + off, 0, n_embd * ggml_element_size(tensors[il]));
+        }
+    }
+
+    return true;
+}
